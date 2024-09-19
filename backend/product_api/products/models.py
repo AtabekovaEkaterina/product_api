e39e55888948b12aed01d8a752695131661e9f08
Atabekova Ekaterina <katerina19870405@yandex.ru>
2024-09-19 10:02:13 +0300

    new migrations

--- backend/product_api/products/models.py
+++ backend/product_api/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
     description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=3)
 
